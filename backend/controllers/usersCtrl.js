e5138eb38199f9509f73b67bb7a7562ca5a47616
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-25 10:13:26 +0100

    Add usersController with register and login functionalities
    - Implement `register` method to handle user registration:
      - Validate input fields (username, email, password).
      - Check if a user with the provided email already exists.
      - Hash the password using bcrypt and save the new user in the database.
      - Respond with the created user’s username, email, and ID.
    
    - Implement `login` method to handle user login:
      - Validate email and password input fields.
      - Check if the user exists in the database.
      - Compare the provided password with the hashed password stored in the database using bcrypt.
      - Generate a JWT token upon successful authentication and send it along with user details.
    
    - Use `express-async-handler` to handle errors in both routes.

---
+++ backend/controllers/usersCtrl.js
@@ -0,0 +1,76 @@
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../model/User");
+
+const usersController = {
+    register: asyncHandler(async(req, res) => {
+        const {username, email, password} = req.body;
+
+        if(!username || !email || !password) {
+            throw new Error("Please all fields are required");
+        }
+
+        // ! check if user already exists
+        const userExists  = await User.findOne({email});
+        if(userExists) {
+            throw new Error("User already exist");
+        }
+        // ! Hash the password
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // ! Create user and save to DB
+
+        const userCreated = await User.create({
+            email,
+            username,
+            password:hashedPassword
+        });
+
+        // send the response
+        res.json({
+            username: userCreated.username,
+            email: userCreated.email,
+            id:userCreated._id
+        })
+
+
+    }),
+
+
+
+    // !login
+    login: asyncHandler(async(req, res) => {
+        // get the user data
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            throw new Error("Invalid login credentials");
+        };
+
+        // compare user password
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            throw new Error("Invalid login credentials");
+        }
+
+        // ! Generate Token 
+        const token = jwt.sign({id: user._id}, 'howareyoudoing', {
+            expiresIn: "30d"
+        });
+
+        // send the response
+        res.json({
+            message: 'Login success',
+            token,
+            id: user._id,
+            email: user.email,
+            username: user.username
+        });
+    })
+}
+module.exports = usersController;
