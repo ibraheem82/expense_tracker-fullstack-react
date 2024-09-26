c47d97812d022b56ca012cfcb160d423581f209c
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-26 03:46:02 +0100

    feat: add transaction routes with authentication
    - Add route for creating new transactions, protected by `isAuthenticated` middleware
    - Add route for listing user-specific transactions
    - Integrate transaction controller methods to handle requests

---
+++ backend/routes/transactionRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const isAuthenticated = require('../middlewares/isAuth');
+const transactionController = require('../controllers/transactionCtrl');
+
+
+const transactionRouter = express.Router();
+
+// Create
+transactionRouter.post("/api/v1/transactions/create", isAuthenticated, transactionController.create)
+
+// lists
+transactionRouter.get("/api/v1/transactions/lists", isAuthenticated, transactionController.lists)
+
+module.exports = transactionRouter;
