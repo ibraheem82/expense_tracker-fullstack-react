4cbccebac3e5c6e9f988ed02a63f0fb4da77bb37
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-26 02:33:35 +0100

    feat: add error handling middleware
    - Implement `errHandler` middleware to manage server errors
    - Set default status code to 500 if no error status is defined

--- backend/middlewares/errHandlerMiddleware.js
+++ backend/middlewares/errHandlerMiddleware.js
@@ -1,4 +1,5 @@
 const errHandler = (err, req, res, next) => {
+    const statusCode =  res.statusCode === 200 ? 500 : res.statusCode;
     res.json({
         message: err.message,
         stack:err.stack
