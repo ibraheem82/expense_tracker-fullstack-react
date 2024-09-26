f4dcc4e46795bed9d5b2cedf7f6180fe1d6a8c92
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-26 09:10:07 +0100

    ....

--- backend/controllers/transactionCtrl.js
+++ backend/controllers/transactionCtrl.js
@@ -1,5 +1,4 @@
 const asyncHandler = require("express-async-handler");
-const Category = require("../model/Category");
 const Transaction = require("../model/Transaction");
 
 const transactionController = {
