12d50fe6bb60bb928756595afc5f35dbb5ff5752
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-26 04:48:20 +0100

    feat: implement filtered transaction retrieval
    - Add `getFilteredTransactions` method to filter transactions based on date range, type, and category
    - Allow filtering by `startDate`, `endDate`, `type`, and handle special cases for `category` ("All" and "Uncategorized")
    - Sort transactions by date in descending order

--- backend/controllers/transactionCtrl.js
+++ backend/controllers/transactionCtrl.js
@@ -27,11 +27,48 @@ const transactionController = {
 
 
     // *Lists
-    lists: asyncHandler(async (req, res) => {
-        const transactions = await Transaction.find({user:req.user});
-        res.json(transactions);
-       
+    getFilteredTransactions: asyncHandler(async (req, res) => {
+        const { startDate, endDate, type, category } = req.query;
+
+        // A filters object is created to store filtering criteria.
+        //     It's initialized with the user ID (req.user). This ensures only transactions belonging to the logged-in user are retrieved.
+        let filters = { user: req.user }
+
+//         Date Range Filter:
+// If startDate is provided, filters transactions with dates greater than or equal to the start date.
+// If endDate is provided, filters transactions with dates less than or equal to the end date.
+
+// If startDate exists, it adds a filter to the filters object for the date field using the spread syntax (...).This ensures other potential filters on date are preserved.
+        if (startDate) {
+            filters.date = { ...filters.date, $gte: new Date(startDate) };
+        }
+
+        if (endDate) {
+            filters.date = { ...filters.date, $lte: new Date(endDate) };
+        }
+
+        if (type) {
+            // If type is provided, filters transactions by the specified type.
+            filters.type = type;
+        }
+
+
+        if (category) {
+            if (category === "All") {
+                //! No category filter needed when filtering for 'All' 
+            } else if(category === "Uncategorized"){
+                // filter for transaction that are specifically categorized as 'Uncategorized'
+                filters.category = 'Uncategorized';
+            } else {
+                filters.category = category
+            }
+        }
+        const transactions = await Transaction.find(filters).sort({date: -1});
+        res.json(transactions)
+
     }),
+
+
 
     update: asyncHandler(async (req, res) => {
     }),
