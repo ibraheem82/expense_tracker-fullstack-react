2046247e37fe82a05967b7cb0bf55152526fc403
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-10-14 13:22:57 +0100

    rendering of user navbar based on authentication state... login or logout state....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,19 @@ import HeroSection from "./components/Home/HomePage";
 import PublicNavbar from "./components/Navbar/PublicNavbar";
 import LoginForm from "./components/Users/Login";
 import RegistrationForm from "./components/Users/Register";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar"
+import { getUserFromStorage } from "./utils/getUserFromStorage";
+import { useSelector } from "react-redux";
+
 
 function App() {
-
+const user = useSelector((state) => state?.auth?.user);
 
   return (
     <BrowserRouter>
     {/* NavBar */}
-    <PublicNavbar />
+    
+    {user ? <PrivateNavbar /> : <PublicNavbar />}
     <Routes>
     <Route path="/" element={<HeroSection />} />
     <Route path="/login" element={<LoginForm />} />
