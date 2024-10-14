74efe07f129749bc70a68c0f8e1b65d67f06a0e8
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-10-14 02:09:52 +0100

    port was changed to 5000

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const userRouter = require('./routes/userRouter');
-const errHandler = require('./middlewares/errHandlerMiddleware');
-const categoryRouter = require('./routes/categoryRouter');
-const transactionRouter = require('./routes/transactionRouter');
+const userRouter = require("./routes/userRouter");
+const errHandler = require("./middlewares/errHandlerMiddleware");
+const categoryRouter = require("./routes/categoryRouter");
+const transactionRouter = require("./routes/transactionRouter");
 const cors = require("cors");
-
 
 const app = express();
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("DB Connected"))
-    .catch((e) => console.log(e));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connected"))
+  .catch((e) => console.log(e));
 // ! Cors config
 
 const corsOptions = {
-    origin: ["https://5173-ibraheem82-fullstackexp-c2iib4cfprr.ws-eu116.gitpod.io/"]
-}
-
-
+  origin: ["http://localhost:5173"],
+};
 
 app.use(cors(corsOptions));
-    // Middlewares 
-app.use(express.json())
-const PORT = process.env.PORT || 6000;
-
+// Middlewares
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 // Routes
 app.use("/", userRouter);
 app.use("/", categoryRouter);
 app.use("/", transactionRouter);
 
-
 // Err handler
 app.use(errHandler);
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.....`);
-    });
+  console.log(`Server is running on port ${PORT}.....`);
+});
