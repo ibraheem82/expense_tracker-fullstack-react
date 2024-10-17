4f1a9d896746ddcb93721a87c860626bbff28a4f
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-10-17 13:01:37 +0100

    feat(App): implement conditional navbar rendering based on user authentication status and define main application routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,25 @@ import RegistrationForm from "./components/Users/Register";
 import PrivateNavbar from "./components/Navbar/PrivateNavbar"
 import { getUserFromStorage } from "./utils/getUserFromStorage";
 import { useSelector } from "react-redux";
+import AddCategory from "./components/Category/AddCategory";
+import CategoriesList from "./components/Category/CategoriesList";
 
 
 function App() {
-const user = useSelector((state) => state?.auth?.user);
+  const user = useSelector((state) => state?.auth?.user);
 
   return (
     <BrowserRouter>
-    {/* NavBar */}
-    
-    {user ? <PrivateNavbar /> : <PublicNavbar />}
-    <Routes>
-    <Route path="/" element={<HeroSection />} />
-    <Route path="/login" element={<LoginForm />} />
-    <Route path="/register" element={<RegistrationForm />} />
-    </Routes>
+      {/* NavBar */}
+
+      {user ? <PrivateNavbar /> : <PublicNavbar />}
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/categories" element={<CategoriesList />} />
+      </Routes>
     </BrowserRouter>
   )
 }
