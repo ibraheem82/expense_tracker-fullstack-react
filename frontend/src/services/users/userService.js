d669275f59ddcd768f2277413f45f8a792a78fd3
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-10-28 10:31:53 +0100

    maked the token accessible to the nessecary services....

--- frontend/src/services/users/userService.js
+++ frontend/src/services/users/userService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { BASE_URL } from "../../utils/url";
-// import { getUserFromStorage } from "../../utils/getUserFromStorage";
+import { getUserFromStorage } from "../../utils/getUserFromStorage";
 //! Get the token
-// const token = getUserFromStorage();
+const token = getUserFromStorage();
 //! Login
 export const loginAPI = async ({ email, password }) => {
   const response = await axios.post(`${BASE_URL}/users/login`, {
